2257f15376ccc335822a81239688cbfe09ebfee2
asanold <Simonkoskin@mail.ru>
2022-12-21 00:42:18 +0300

    add lab1

---
+++ labs/lab1/Template_Method/PDFDataMiner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.Template_Method
+{
+    class PDFDataMiner : DataMiner
+    {
+        public override string openFile()
+        {
+            return "openFile";
+        }
+
+        public override string extractData()
+        {
+            return "extractData";
+        }
+
+        public override string parseData()
+        {
+            return "parseData";
+        }
+
+        public override string closeFile()
+        {
+            return "closeFile";
+        } 
+    }
+}
