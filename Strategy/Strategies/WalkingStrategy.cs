4519d7d56d0d0ec9542c2bf8001f18899d31ad56
asanold <Simonkoskin@mail.ru>
2022-12-20 12:08:23 +0300

    Добавьте файлы проекта.

---
+++ Strategy/Strategies/WalkingStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.Strategy.Strategies
+{
+    class WalkingStrategy : IRouteStrategy
+    {
+        string type = "Walking";
+
+        public string BuildRoute(string A, string B)
+        {
+            return type + " " + A + " " + B;
+        }
+    }
+}
